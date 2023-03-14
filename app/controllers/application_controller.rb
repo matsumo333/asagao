2855aaa489b8e5c2377cc9f4c05dbf9a0ddf5709
matsumo333 <matsumo333@icloud.com>
2023-03-14 21:39:37 +0900

    326

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  private def current_member
+    Member.find_by(id: session[:member_id]) if session[:member_id]
+  end
+  helper_method :current_member
+
+  class LoginRequired < StandardError; end
+  class Forbidden < StandardError; end
+
+  private def login_required
+    raise LoginRequired unless current_member
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230314070725) do
+ActiveRecord::Schema.define(version: 20230314102629) do
 
   create_table "members", force: :cascade do |t|
     t.integer "number", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20230314070725) do
     t.boolean "administrator", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
