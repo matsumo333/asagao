0792de50e4b5127836144cd4f6f7f9d1662faeb7
matsumo333 <matsumo333@icloud.com>
2023-03-14 23:45:37 +0900

    p340

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,65 @@ class ArticlesController < ApplicationController
 
   # 記事一覧
   def index
-    @articles = Article.visible.order(released_at: :desc)
+    @articles = Article.order(released_at: :desc)
+
     @articles = @articles.open_to_the_public unless current_member
-    @articles = @articles.page(params[:page]).per(5)
+
+    # unless current_member&.administrator?
+      # @articles = @articles.visible
+    # end
+
+    # @articles = @articles.page(params[:page]).per(5)
   end
 
   # 記事詳細
   def show
-    @articles = Article.visible
-    @articles = @articles.open_to_the_public unless current_member
-    @article = @articles.find(params[:id])
+    # articles = Article.all
+# 
+    # articles = articles.open_to_the_public unless current_member
+# 
+    # unless current_member&.administrator?
+      # articles = articles.visible
+    # end
+
+    @article = Article.find(params[:id])
+  end
+
+  # 新規登録フォーム
+  def new
+    @article = Article.new
+  end
+
+  # 編集フォーム
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # 新規作成
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to @article, notice: "ニュース記事を登録しました。"
+    else
+      render "new"
+    end
+  end
+
+  # 更新
+  def update
+    @article = Article.find(params[:id])
+    @article.assign_attributes(params[:article])
+    if @article.save
+      redirect_to @article, notice: "ニュース記事を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  # 削除
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to :articles
   end
 end
